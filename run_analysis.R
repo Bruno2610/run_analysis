7d0dd48e187617eea76d18b39e380b356beac805
Bruno2610 <bruno.dan.1510@gmail.com>
2016-03-04 19:01:43 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,37 @@
 #course project:
 
-#reading test archives
-s_test<-read.table("subject_test.txt")
-x_test<-read.table("x_test.txt")
-y_test<-read.table("y_test.txt")
-feat<-read.table("features.txt")
-names<-as.character(feat[,2])
-names<-gsub("tBody"," ",names)
-names<-gsub("tGravity"," ",names)
-names<-gsub("fBodyAcc"," ",names)
-names<-gsub("fBodyGyro"," ",names)
-names<-gsub("fBodyBodyGyro"," ",names)
-names<-gsub("fBodyBody"," ",names)
-names<-gsub("^ -","",names)
-names<-gsub("^ ","",names)
-
-#reading train archives
-s_train<-read.table("subject_train.txt")
-x_train<-read.table("x_train.txt")
-y_train<-read.table("y_train.txt")
-
-#puts the data together
-test<-cbind(s_test,x_test,y_test)
-train<-cbind(s_train,x_train,y_train)
-#number of columns
-ntest<-ncol(test)
-ntrain<-ncol(train)
-#calculates means and standard deviations
-means_test<-apply(test[2:ntest-1],2,mean)
-means_train<-apply(train[2:ntrain-1],2,mean)
-sd_test<-apply(test[2:ntest-1],2,sd)
-sd_train<-apply(train[2:ntrain-1],2,sd)
-means<-cbind(means_test,means_train)
-write.table(means,file="./means",row.names=FALSE,col.names=TRUE)
-
-
+#load test data  
+subject_test<-read.table("subject_test.txt")
+X_test<-read.table("X_test.txt")
+Y_test<-read.table("Y_test.txt")
+  
+#load training data
+subject_train<-read.table("subject_train.txt")
+X_train<-read.table("X_train.txt")
+Y_train<-read.table("Y_train.txt")
+  
+#load lookup information
+features<-read.table("features.txt", col.names=c("featureId", "featureLabel"))
+activities<-read.table("activity_labels.txt", col.names=c("activityId", "activityLabel"))
+activities$activityLabel<-gsub("_", "", as.character(activities$activityLabel))
+includedFeatures<-grep("-mean\\(\\)|-std\\(\\)", features$featureLabel)
+  
+#merge test and training data and name then
+subject<-rbind(subject_test, subject_train)
+names(subject)<-"subjectId"
+X<-rbind(X_test, X_train)
+X<-X[, includedFeatures]
+names(X)<-gsub("\\(|\\)", "", features$featureLabel[includedFeatures])
+Y<-rbind(Y_test, Y_train)
+names(Y)<-"activityId"
+activity<-merge(Y, activities, by="activityId")$activityLabel
+  
+#merge data frames of different columns to form one data table
+data<-cbind(subject, X, activity)
+write.table(data, "merged_tidy_data.txt")
+  
+#create a dataset grouped by subject and activity with the recquired information (mean and sd)
+library(data.table)
+dataDT<-data.table(data)
+calculatedData<-dataDT[, lapply(.SD, mean), by=c("subjectId", "activity")]
+write.table(calculatedData, "calculated_data.txt")
